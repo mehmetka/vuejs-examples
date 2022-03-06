f4724f4788de90197e0e2d6b1eda3305c8af0880
Mehmet Kanat <mehmet.kanat@protonmail.com>
2022-03-06 18:09:48 +0300

    Add build-a-bot project init

---
+++ build-a-bot/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <main>
+    <RobotBuilder />
+  </main>
+</template>
+
+<script>
+import RobotBuilder from './build/RobotBuilder.vue';
+
+export default {
+  name: 'App',
+  components: {
+    RobotBuilder,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
