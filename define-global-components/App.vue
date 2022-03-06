86173a853755d5882b559d0b75d4e1f021e40fdb
Mehmet Kanat <mehmet.kanat@protonmail.com>
2022-03-06 17:01:04 +0300

    Add defining global components example and notes

---
+++ define-global-components/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <main>
+    <HomePage />
+  </main>
+</template>
+
+<script>
+import HomePage from './components/HomePage.vue';
+
+export default {
+  name: 'App',
+  // No longer have to register it here.
+  // components: {
+  //   HomePage,
+  // },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
