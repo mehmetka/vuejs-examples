ddbe8d92fa8058b48f6a01f8659a6044eef5d0ee
Mehmet Kanat <mehmet.kanat@protonmail.com>
2022-03-08 22:09:11 +0300

    reformat

--- build-a-bot/src/main.js
+++ build-a-bot/src/main.js
@@ -2,5 +2,5 @@ import { createApp } from 'vue';
 import App from './App.vue';
 
 // A vue instance or vue app is created whenever you call Vue.createApp()
-// Only have one Vue instance 
+// Only have one Vue instance
 createApp(App).mount('#app');
