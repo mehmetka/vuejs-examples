e8dca4bf6eae6489f3df5b164a28ab241a16dbd2
Mehmet Kanat <mehmet.kanat@protonmail.com>
2022-03-09 23:14:08 +0300

    Add notes and some css

--- build-a-bot/src/App.vue
+++ build-a-bot/src/App.vue
@@ -1,4 +1,7 @@
 <template>
+  <header>
+    Header
+  </header>
   <main>
     <RobotBuilder />
   </main>
@@ -16,12 +19,24 @@ export default {
 </script>
 
 <style>
+body {
+  background: linear-gradient(to bottom, #555, #999);
+  background-attachment: fixed;
+}
+</style>
+
+<!-- We can have more than one style element. -->
+<style scoped> /* Only applied to App component. */
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+main {
+  margin: 0 auto;
+  padding: 30px;
+  background-color: white;
+  width: 1024px;
+  min-height: 300px;
 }
 </style>
