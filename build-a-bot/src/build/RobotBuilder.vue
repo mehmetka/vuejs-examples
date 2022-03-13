01f2fc4d4f5fe8454631b53df514816675e6b629
Mehmet Kanat <mehmet.kanat@protonmail.com>
2022-03-13 23:41:02 +0300

    Add builder component

--- build-a-bot/src/build/RobotBuilder.vue
+++ build-a-bot/src/build/RobotBuilder.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="content">
-    <button class="add-to-cart" @click="addToCart()">Add to cart</button>
+    <div class="preview">
+      <div class="preview-content">
+        <div class="top-row">
+          <img :src="selectedRobot.head.src" alt=""/>
+        </div>
+        <div class="middle-row">
+          <img :src="selectedRobot.leftArm.src" class="rotate-left" alt=""/>
+          <img :src="selectedRobot.torso.src" alt=""/>
+          <img :src="selectedRobot.rightArm.src" class="rotate-right" alt=""/>
+        </div>
+        <div class="bottom-row">
+          <img :src="selectedRobot.base.src" alt=""/>
+        </div>
+      </div>
+      <button class="add-to-cart" @click="addToCart()">Add to cart</button>
+    </div>
     <div class="top-row">
       <!--
       <div :class="[saleBorderClass, 'top', 'part']">
@@ -205,8 +220,7 @@ export default {
 }
 .add-to-cart {
   position: absolute;
-  right: 30px;
-  width: 220px;
+  width: 210px;
   padding: 3px;
   font-size: 16px;
 }
@@ -220,5 +234,26 @@ td th {
 }
 .sale-border {
   border: 3px solid red;
+}
+.preview {
+  position: absolute;
+  top: -20px;
+  right: 0;
+  width: 210px;
+  height: 210px;
+  padding: 5px;
+}
+.preview-content {
+  border: 1px solid #999;
+}
+.preview img {
+  width: 50px;
+  height: 50px;
+}
+.rotate-right {
+  transform: rotate(90deg);
+}
+.rotate-left {
+  transform: rotate(-90deg);
 }
 </style>
