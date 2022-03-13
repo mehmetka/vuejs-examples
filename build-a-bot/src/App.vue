e03712f3d74b65c1223028f7c9f89b5a1da7a5e9
Mehmet Kanat <mehmet.kanat@protonmail.com>
2022-03-14 00:59:43 +0300

    Add header

--- build-a-bot/src/App.vue
+++ build-a-bot/src/App.vue
@@ -1,7 +1,14 @@
 <template>
-  <!-- <header>
-    Header
-  </header> -->
+  <header>
+    <nav>
+      <ul>
+        <li class="nav-item">
+          <img class="logo" src="./assets/build-a-bot-logo.png" alt=""/> Build-a-bot
+        </li>
+        User: {{userName}}
+      </ul>
+    </nav>
+  </header>
   <main>
     <RobotBuilder />
     <!-- <SearchComponent /> -->
@@ -41,5 +48,24 @@ main {
   background-color: white;
   width: 1024px;
   min-height: 300px;
+}
+header {
+  background-color: #999;
+  width: 1084px;
+  margin: 0 auto;
+}
+ul {
+  padding: 3px;
+  display: flex;
+}
+.nav-item {
+  display: inline-block;
+  padding: 5px 10px;
+  font-size: 22px;
+  border-right: 1px solid #bbb;
+}
+.logo {
+  vertical-align: middle;
+  height: 30px;
 }
 </style>
